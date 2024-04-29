6bfc8c20548e3a25b45aa4d9be3eb41a6936ca5e
Sumitha <124781919+sumithachinnadurai@users.noreply.github.com>
2024-04-29 15:10:33 +0530

    Add files via upload

--- ccdemo2/src/test/java/com/example/AppTest.java
+++ ccdemo2/src/test/java/com/example/AppTest.java
@@ -1,83 +1,123 @@
 package com.example;
 
+
+import java.io.FileInputStream;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+// 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class AppTest {
-    static WebDriver dri;
+public class AppTest 
+{
+    public static Logger log = LogManager.getLogger(AppTest.class);
+    WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
 
-    @Test(enabled = false)
-    public void Testcase1() throws Exception {
+    @BeforeMethod
+    public void mybfre() throws Exception
+    {
+        driver=new ChromeDriver();
         WebDriverManager.chromedriver().setup();
-        dri = new ChromeDriver();
-        dri.get("https://www.barnesandnoble.com");
-        dri.manage().window().maximize();
-        dri.findElement(By.xpath("//*[@id=\"rhf_header_element\"]/nav/div/div[3]/form/div/div[1]/a")).click();
-        dri.findElement(By.linkText("Books")).click();
-        WebElement sear = dri
-                .findElement(By.xpath("//*[@id=\"rhf_header_element\"]/nav/div/div[3]/form/div/div[2]/div/input[1]"));
-        sear.sendKeys("Chetan Bhagat");
-        sear.sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-        String str = dri
-                .findElement(By.xpath("//*[@id=\"searchGrid\"]/div/section[1]/section[1]/div/div[1]/div[1]/h1/span"))
-                .getText();
-        if (str.equals("Chetan Bhagat")) {
-            System.out.println("The text matches");
-        } else {
-            System.out.println("The text doesnot match");
-        }
-        dri.close();
+        ExtentSparkReporter r = new ExtentSparkReporter("D:\\CC2report.html");
+        report = new ExtentReports();
+        report.attachReporter(r);
+
+        log.info("URL is being opened!!!");
+        driver.get("https://www.barnesandnoble.com/");
+        log.info("Successfully opened!");
+         PropertyConfigurator.configure("C:\\Users\\sumit\\OneDrive\\Desktop\\ccdemo2\\src\\resources\\log4j.properties");
+
+    }
+    
+    @Test(priority = 0)
+    public void Test1() throws Exception
+    {
+        FileInputStream fs = new FileInputStream("D:\\Login.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fs);
+        XSSFSheet sheet = workbook.getSheet("Login");
+        XSSFRow row = sheet.getRow(1);
+        String name = row.getCell(0).getStringCellValue(); 
+        driver.findElement(By.xpath("//*[@id=\'rhf_header_element\']/nav/div/div[3]/form/div/div[1]/a")).click();
+        driver.findElement(By.xpath("//*[@id='rhf_header_element']/nav/div/div[3]/form/div/div[1]/div/a[2]")).click();
+        driver.findElement(By.xpath("//*[@id='rhf_header_element']/nav/div/div[3]/form/div/div[2]/div/input[1]")).sendKeys(name);
+        // test.log(Status.PASS,"passed");
+        driver.findElement(By.xpath("//*[@id='rhf_header_element']/nav/div/div[3]/form/div/span/button")).click();
+        WebElement check1=driver.findElement(By.xpath("//*[@id='searchGrid']/div/section[1]/section[1]/div/div[1]/div[1]/h1"));
+        // Assert.assertTrue(check1.getText().contains("chetan bhagat"), "Name not found!");
+
+        log.info("TestCase 1 passed Successfully!");
     }
 
-    @Test
-    public void TestCase2() throws Exception {
-        WebDriverManager.chromedriver().setup();
-        dri = new ChromeDriver();
-        dri.get("https://www.barnesandnoble.com");
-        dri.manage().window().maximize();
 
-        WebElement hov = dri.findElement(By.xpath("//*[@id=\"rhfCategoryFlyout_Audiobooks\"]"));
-        new Actions(dri).moveToElement(hov).perform();
-        dri.findElement(
-                By.xpath("//*[@id=\"navbarSupportedContent\"]/div/ul/li[5]/div/div/div[1]/div/div[2]/div[1]/dd/a[1]")).click();
+    @Test(priority = 1)
+    public void Test2() throws InterruptedException {
+        WebElement audiobooks = driver.findElement(By.linkText("Audiobooks"));
         Thread.sleep(2000);
-        dri.findElement(By.xpath("//*[@id=\"addToBagForm_2940178667866\"]/input[11]")).click();
-        dri.findElement(By.linkText("Item Successfully Added To Your Cart")).click();
-        dri.close();
+        Actions action = new Actions(driver);
+        action.moveToElement(audiobooks).perform();
+        driver.findElement(By.xpath("//*[@id='navbarSupportedContent']/div/ul/li[5]/div/div/div[1]/div/div[2]/div[1]/dd/a[1]")).click();
+        Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,300)");
+        driver.findElement(By.linkText("Funny Story")).click();
+        Thread.sleep(2000);
+        js.executeScript("window.scrollBy(0,500)");
+        driver.findElement(By.xpath("//*[@id='otherAvailFormats']/div/div")).click();
+        Thread.sleep(2000);
+        js.executeScript("window.scrollBy(0,600)");
+         driver.findElement(By.xpath("//*[@id='prodInfoContainer']/div[3]/form[1]/input[11]")).click();
+        // Thread.sleep(2000);
+        // WebElement cart = driver.findElement(By.xpath("//*[@id='add-to-bag-main']/div[1]"));
+        // String add = cart.getText();
+        // if (add.equals("Item Successfully Added To Your cart")) log.info("TestCase 2 Executed Successfully!");
+        // else
+        //     log.error("Error Occurred!");
+        Thread.sleep(2000);
     }
 
-    @Test(enabled = false)
-    public void Testcase3() throws Exception {
-        WebDriverManager.chromedriver().setup();
-        dri = new ChromeDriver();
-        dri.get("https://www.barnesandnoble.com");
-        dri.manage().window().maximize();
-        JavascriptExecutor js = (JavascriptExecutor) dri;
 
-        js.executeScript("window.scrollBy(0,1800)");
-        dri.findElement(By.xpath("/html/body/main/div[3]/div[3]/div/section/div/div/div/div/div/a[1]/span")).click();
-        js.executeScript("window.scrollBy(0,2000)");
-        dri.findElement(By.xpath("//*[@id=\"rewards-modal-link\"]")).click();
-        dri.findElement(By.xpath("//*[@id=\"loginForm\"]/div[1]/div[1]/div/div/label")).click();
-        WebElement ema = dri.findElement(By.className("user-error"));
-        ema.sendKeys("abbdas");
-        dri.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("dhjadfj");
-        dri.findElement(By.xpath("//*[@id=\"loginForm\"]/div[1]/div[4]/div/div/button")).click();
-        String str = dri.findElement(By.xpath("//*[@id=\"mloginErrors\"]/em")).getText();
-        if (str.equals("Please enter a valid email address")) {
-            System.out.println("Mail id is checked properly");
-        } else
-            System.out.println("Mail id is not checked properly");
-        dri.close();
+    @Test(priority = 2)
+    public void Test3() throws Exception{
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1100)");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id='footer']/div/dd/div/div/div[1]/div/a[5]/span")).click();
+        Thread.sleep(2000);
+        js.executeScript("windows.scrollBy(0,400)");
+        driver.findElement(By.xpath("//*[@id='rewards-modal-link']")).click();
+        WebElement check2=driver.findElement(By.xpath("//*[@id=\"dialog-title\"]"));
+        Assert.assertTrue(check2.getText().contains("Sign in or Create an Account"), "Sign in first!!");
+        log.info("TestCase 3 passed Successfully!");
     }
 
+
+    @AfterMethod
+    public void myaftrmthd() throws Exception{
+        log.info("Tests Executed!!!");
+        report.flush();
+
+        driver.quit();
+    }
 }
